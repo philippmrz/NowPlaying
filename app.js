cae3585684b182c86a13b317d24781f7e5e3f8ae
Philipp Merz <phmrch@gmail.com>
2019-08-11 21:22:01 +0200

    Updated js logic, getting currently playing

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-(function() {
-  let API_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing";
-  let API_AUTH = "https://accounts.spotify.com/authorize";
-  let CLIENT_ID = "0de75d8a42984939959b9392363bcd50";
-  let SCOPE = "user-read-currently-playing";
+let API_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing";
+let API_AUTH = "https://accounts.spotify.com/authorize";
+let CLIENT_ID = "0de75d8a42984939959b9392363bcd50";
+let SCOPE = "user-read-currently-playing";
 
+const hash = window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce(function (initial, item) {
+      if (item) {
+          var parts = item.split('=');
+          initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+  }, {});
 
-  const hash = window.location.hash
-    .substring(1)
-    .split('&')
-    .reduce(function (initial, item) {
-        if (item) {
-            var parts = item.split('=');
-            initial[parts[0]] = decodeURIComponent(parts[1]);
-        }
-        return initial;
-    }, {});
-    window.location.hash = '';
-    console.log(hash.access_token);
+if (!hash.access_token) {
+  window.location.replace(API_AUTH + '?response_type=token' + '&client_id=' + CLIENT_ID +
+  '&scope=' + encodeURIComponent(SCOPE) +
+  '&redirect_uri=' + encodeURIComponent("http://localhost:8000/"));
+}
 
-    if (!hash.access_token) {
-      window.location.replace(API_AUTH + '?response_type=token' + '&client_id=' + CLIENT_ID +
-      '&scope=' + encodeURIComponent(SCOPE) +
-      '&redirect_uri=' + encodeURIComponent("http://localhost:8000/"));
-    }
-}())
+let HTTP = new XMLHttpRequest();
+HTTP.open("GET", API_ENDPOINT);
+HTTP.setRequestHeader("Authorization", 'Bearer ' + hash.access_token);
+HTTP.send();
+HTTP.onreadystatechange = (e) => {
+  if (HTTP.readyState === 4) console.log(HTTP.responseText);
+}
