ed88029f13e5c3b2a8ce47feaa441f588fda42ae
Philipp Merz <phmrch@gmail.com>
2019-08-11 21:44:51 +0200

    Added cover image, name and artist info

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let API_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing";
-let API_AUTH = "https://accounts.spotify.com/authorize";
-let CLIENT_ID = "0de75d8a42984939959b9392363bcd50";
-let SCOPE = "user-read-currently-playing";
+const API_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing";
+const API_AUTH = "https://accounts.spotify.com/authorize";
+const CLIENT_ID = "0de75d8a42984939959b9392363bcd50";
+const SCOPE = "user-read-currently-playing";
+const SONG_DISPLAY = document.querySelector('#song-info');
 
 const hash = window.location.hash
   .substring(1)
@@ -25,5 +26,10 @@ HTTP.open("GET", API_ENDPOINT);
 HTTP.setRequestHeader("Authorization", 'Bearer ' + hash.access_token);
 HTTP.send();
 HTTP.onreadystatechange = (e) => {
-  if (HTTP.readyState === 4) console.log(HTTP.responseText);
+  if (HTTP.readyState === 4) {
+    let songData = JSON.parse(HTTP.responseText);
+    let SONG_INFO = "You're now listening to " + songData.item.name + " by " + songData.item.artists[0].name;
+    SONG_DISPLAY.innerHTML = SONG_INFO;
+    document.querySelector("#cover").src = songData.item.album.images[0].url;
+  }
 }
