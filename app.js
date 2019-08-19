b5294a32f068a5d2af14e68052139d6a176eb810
Philipp Merz <phmrch@gmail.com>
2019-08-19 23:02:36 +0200

    Added automatic, optimized page reload and refractored

--- app.js
+++ app.js
@@ -7,63 +7,93 @@
   const ARTIST_NAME_P = document.querySelector('#artist-name');
   const COVER = document.querySelector("#cover");
 
-  const hash = window.location.hash
-    .substring(1)
-    .split('&')
-    .reduce(function (initial, item) {
-        if (item) {
-            var parts = item.split('=');
-            initial[parts[0]] = decodeURIComponent(parts[1]);
-        }
-        return initial;
-    }, {});
-  window.location.hash = "";
 
+  const hash = extractHash();
+
+  //Redirect client to Spotify for authentication if not done already
   if (!hash.access_token) {
     window.location.replace(API_AUTH + '?response_type=token' + '&client_id=' + CLIENT_ID +
     '&scope=' + encodeURIComponent(SCOPE) +
-    '&redirect_uri=' + encodeURIComponent("https://philippmrz.github.io/NowPlaying/"));
+    '&redirect_uri=' + encodeURIComponent("http://localhost:8000/"));
+  }
+  setInterval(updateSong, 5000);
+
+
+  function updateSong() {
+    let req = requestSongInfo();
+
+    req.onreadystatechange = (e) => {
+        if (req.readyState === 4 && req.status == 200) buildPage(JSON.parse(req.responseText));
+    };
   }
 
-  let HTTP = new XMLHttpRequest();
-  HTTP.open("GET", API_ENDPOINT);
-  HTTP.setRequestHeader("Authorization", 'Bearer ' + hash.access_token);
-  HTTP.send();
-  HTTP.onreadystatechange = (e) => {
-    if (HTTP.readyState === 4) {
-      let songData = JSON.parse(HTTP.responseText);
-      let songName = songData.item.name;
-      let artistName = songData.item.artists[0].name;
+
+  function extractHash(){
+    const hash = window.location.hash
+      .substring(1)
+      .split('&')
+      .reduce(function (initial, item) {
+          if (item) {
+              var parts = item.split('=');
+              initial[parts[0]] = decodeURIComponent(parts[1]);
+          }
+          return initial;
+      }, {});
+    window.location.hash = "";
+    return hash;
+  }
+
+  function requestSongInfo() {
+    let HTTP = new XMLHttpRequest();
+    HTTP.open("GET", API_ENDPOINT);
+    HTTP.setRequestHeader("Authorization", 'Bearer ' + hash.access_token);
+    HTTP.send();
+    return HTTP;
+  }
+
+  function buildPage(songData) {
+    let songName = songData.item.name;
+    let artistName = songData.item.artists[0].name;
+    if (songName != SONG_NAME_P.innerHTML && artistName != ARTIST_NAME_P) {
+      //Fill appropriate html elements with information from API
       SONG_NAME_P.innerHTML = songName;
       ARTIST_NAME_P.innerHTML = artistName;
       COVER.src = songData.item.album.images[0].url;
+
+      //Let vibrant.js extract colors from cover image and use these colors to color the page
       COVER.addEventListener('load', function() {
         let vibrant = new Vibrant(COVER);
         let swatches = vibrant.swatches();
-        let grad = "linear-gradient(" + swatches["DarkVibrant"].getHex() + ", " + swatches["Vibrant"].getHex() + ")";
-        document.querySelector("body").style.background = grad;
-        document.querySelector("body").style.color = swatches["LightVibrant"].getHex();
-
-        document.querySelector("#vibrant").style.backgroundColor = swatches["Vibrant"].getHex();
-        document.querySelector("#muted").style.backgroundColor = swatches["Muted"].getHex();
-        document.querySelector("#dark-vibrant").style.backgroundColor = swatches["DarkVibrant"].getHex();
-        document.querySelector("#dark-muted").style.backgroundColor = swatches["DarkMuted"].getHex();
-        document.querySelector("#light-vibrant").style.backgroundColor = swatches["LightVibrant"].getHex();
-
-        for (let swatch in swatches) {
-          if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
-              console.log(swatch, swatches[swatch].getHex())
-          }
-        }
-     });
+        colorPage(swatches);
+      });
     }
   }
-  let i = 0;
-  document.querySelector("#cover").addEventListener('click', () => {
-    i++;
-    console.log(i);
-    if (i >= 10) {
-      document.querySelector("#color-cont").style.display = "flex";
-    }
-  })
+
+
+  function colorPage(swatches) {
+    const GRAD = "linear-gradient(" + swatches["DarkVibrant"].getHex() + ", " + swatches["Vibrant"].getHex() + ")";
+    document.querySelector("body").style.background = GRAD;
+    document.querySelector("body").style.color = swatches["LightVibrant"].getHex();
+
+    document.querySelector("#vibrant").style.backgroundColor = swatches["Vibrant"].getHex();
+    document.querySelector("#muted").style.backgroundColor = swatches["Muted"].getHex();
+    document.querySelector("#dark-vibrant").style.backgroundColor = swatches["DarkVibrant"].getHex();
+    document.querySelector("#dark-muted").style.backgroundColor = swatches["DarkMuted"].getHex();
+    document.querySelector("#light-vibrant").style.backgroundColor = swatches["LightVibrant"].getHex();
+  }
 }())
+
+
+
+
+
+
+
+
+
+
+let i = 0;
+document.querySelector("#cover").addEventListener('click', () => {
+  i++;
+  if (i >= 10) document.querySelector("#color-cont").style.display = "flex";
+});
